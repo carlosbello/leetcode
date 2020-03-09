c8f6b431cf35e2be104a74a149e17e4a53c0353e
Carlos Bello <bellopauste@yahoo.com>
2020-03-09 19:25:40 +0100

    Add tree builder variations with some optimizations

--- src/main/kotlin/org/carlosbello/datastructures/binarytree/operations.kt
+++ src/main/kotlin/org/carlosbello/datastructures/binarytree/operations.kt
@@ -4,8 +4,11 @@ package org.carlosbello.datastructures.binarytree
  * Construct Binary Tree from Inorder and Postorder Traversal https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
  *
  * Note: The problem statement says we can assume that duplicates do not exist in the tree.
+ *
+ * @see buildFromInorderAndPostorder2 For a first optimization eliminating a repeated task
+ * @see buildFromInorderAndPostorder3 For a second optimization avoiding the creation of copies of subtrees traversals
  */
-fun buildFromInorderAndPostorder(inorder: IntArray, postorder: IntArray) : TreeNode? =
+fun buildFromInorderAndPostorder1(inorder: IntArray, postorder: IntArray) : TreeNode? =
     if (inorder.isEmpty())
         null
     else {
@@ -16,17 +19,71 @@ fun buildFromInorderAndPostorder(inorder: IntArray, postorder: IntArray) : TreeN
         val leftPostorderSubtree = postorder.slice(0..leftInorderSubtree.lastIndex).toIntArray()
         val rightPostorderSubtree = postorder.slice(leftPostorderSubtree.size until postorder.lastIndex).toIntArray()
         TreeNode(rootValue,
-            buildFromInorderAndPostorder(leftInorderSubtree, leftPostorderSubtree),
-            buildFromInorderAndPostorder(rightInorderSubtree, rightPostorderSubtree)
+            buildFromInorderAndPostorder1(leftInorderSubtree, leftPostorderSubtree),
+            buildFromInorderAndPostorder1(rightInorderSubtree, rightPostorderSubtree)
         )
     }
+
+/**
+ * Same as {@link buildFromInorderAndPostorder1} but pre-calculating the indexes of each node value
+ */
+fun buildFromInorderAndPostorder2(inorder: IntArray, postorder: IntArray) : TreeNode? {
+    val inorderValueIndexMap = inorder.withIndex().associate { it.value to it.index }
+
+    fun buildFromInorderAndPostorder(inorder: IntArray, postorder: IntArray) : TreeNode? =
+        if (inorder.isEmpty())
+            null
+        else {
+            val rootValue = postorder.last()
+            val indexOfRootValue = inorderValueIndexMap.getOrDefault(rootValue, -1) - inorderValueIndexMap.getOrDefault(inorder.first(), -1)
+            val leftInorderSubtree = inorder.slice((0 until indexOfRootValue)).toIntArray()
+            val rightInorderSubtree = inorder.slice(indexOfRootValue+1..inorder.lastIndex).toIntArray()
+            val leftPostorderSubtree = postorder.slice(0..leftInorderSubtree.lastIndex).toIntArray()
+            val rightPostorderSubtree = postorder.slice(leftPostorderSubtree.size until postorder.lastIndex).toIntArray()
+            TreeNode(rootValue,
+                buildFromInorderAndPostorder(leftInorderSubtree, leftPostorderSubtree),
+                buildFromInorderAndPostorder(rightInorderSubtree, rightPostorderSubtree)
+            )
+        }
+
+    return buildFromInorderAndPostorder(inorder, postorder)
+}
+
+/**
+ * Same as {@link buildFromInorderAndPostorder2} but avoiding slicing arrays to obtain the subtrees
+ */
+fun buildFromInorderAndPostorder3(inorder: IntArray, postorder: IntArray) : TreeNode? {
+    val inorderValueIndexMap = inorder.withIndex().associate { it.value to it.index }
+    fun sizeOf(subtreeRange: Pair<Int, Int>): Int = subtreeRange.second - subtreeRange.first
+
+    fun buildFromInorderAndPostorder(inorderSubtree: Pair<Int, Int>, postorderSubtree: Pair<Int, Int>) : TreeNode? =
+        if (inorderSubtree.first > inorderSubtree.second)
+            null
+        else {
+            val rootValue = postorder[postorderSubtree.second]
+            val indexOfRootValue = inorderValueIndexMap.getOrDefault(rootValue, -1)
+            val leftInorderSubtree = Pair(inorderSubtree.first, indexOfRootValue - 1)
+            val rightInorderSubtree = Pair(indexOfRootValue + 1, inorderSubtree.second)
+            val leftPostorderSubtree = Pair(postorderSubtree.first, postorderSubtree.first + sizeOf(leftInorderSubtree))
+            val rightPostorderSubtree = Pair(leftPostorderSubtree.second + 1, postorderSubtree.second - 1)
+            TreeNode(rootValue,
+                buildFromInorderAndPostorder(leftInorderSubtree, leftPostorderSubtree),
+                buildFromInorderAndPostorder(rightInorderSubtree, rightPostorderSubtree)
+            )
+        }
+
+    return buildFromInorderAndPostorder(Pair(0, inorder.lastIndex), Pair(0, postorder.lastIndex))
+}
 
 /**
  * Construct Binary Tree from Preorder and Inorder Traversal https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
  *
  * Note: The problem statement says we can assume that duplicates do not exist in the tree.
+ *
+ * @see buildFromPreorderAndInorder2 For a first optimization eliminating a repeated task
+ * @see buildFromPreorderAndInorder3 For a second optimization avoiding the creation of copies of subtrees traversals
  */
-fun buildFromPreorderAndInorder(preorder: IntArray, inorder: IntArray): TreeNode? =
+fun buildFromPreorderAndInorder1(preorder: IntArray, inorder: IntArray): TreeNode? =
     if (inorder.isEmpty())
         null
     else {
@@ -37,7 +94,58 @@ fun buildFromPreorderAndInorder(preorder: IntArray, inorder: IntArray): TreeNode
         val leftPreorderSubtree = preorder.slice(1..leftInorderSubtree.size).toIntArray()
         val rightPreorderSubtree = preorder.slice(leftPreorderSubtree.size+1..preorder.lastIndex).toIntArray()
         TreeNode(rootValue,
-            buildFromPreorderAndInorder(leftPreorderSubtree, leftInorderSubtree),
-            buildFromPreorderAndInorder(rightPreorderSubtree, rightInorderSubtree)
+            buildFromPreorderAndInorder1(leftPreorderSubtree, leftInorderSubtree),
+            buildFromPreorderAndInorder1(rightPreorderSubtree, rightInorderSubtree)
         )
     }
+
+/**
+ * Same as {@link buildFromPreorderAndInorder} but pre-calculating the indexes of each node value
+ */
+fun buildFromPreorderAndInorder2(preorder: IntArray, inorder: IntArray): TreeNode? {
+    val inorderValueIndexMap = inorder.withIndex().associate { it.value to it.index }
+
+    fun buildFromPreorderAndInorder(preorder: IntArray, inorder: IntArray): TreeNode? =
+        if (inorder.isEmpty())
+            null
+        else {
+            val rootValue = preorder.first()
+            val indexOfRootValue = inorderValueIndexMap.getOrDefault(rootValue, -1) - inorderValueIndexMap.getOrDefault(inorder.first(), -1)
+            val leftInorderSubtree = inorder.slice((0 until indexOfRootValue)).toIntArray()
+            val rightInorderSubtree = inorder.slice(indexOfRootValue+1..inorder.lastIndex).toIntArray()
+            val leftPreorderSubtree = preorder.slice(1..leftInorderSubtree.size).toIntArray()
+            val rightPreorderSubtree = preorder.slice(leftPreorderSubtree.size+1..preorder.lastIndex).toIntArray()
+            TreeNode(rootValue,
+                buildFromPreorderAndInorder(leftPreorderSubtree, leftInorderSubtree),
+                buildFromPreorderAndInorder(rightPreorderSubtree, rightInorderSubtree)
+            )
+        }
+
+    return buildFromPreorderAndInorder(preorder, inorder)
+}
+
+/**
+ * Same as {@link buildFromPreorderAndInorder2} but avoiding slicing arrays to obtain the subtrees
+ */
+fun buildFromPreorderAndInorder3(preorder: IntArray, inorder: IntArray) : TreeNode? {
+    val inorderValueIndexMap = inorder.withIndex().associate { it.value to it.index }
+    fun sizeOf(subtreeRange: Pair<Int, Int>): Int = subtreeRange.second - subtreeRange.first
+
+    fun buildFromPreorderAndInorder(preorderSubtree: Pair<Int, Int>, inorderSubtree: Pair<Int, Int>) : TreeNode? =
+        if (inorderSubtree.first > inorderSubtree.second)
+            null
+        else {
+            val rootValue = preorder[preorderSubtree.first]
+            val indexOfRootValue = inorderValueIndexMap.getOrDefault(rootValue, -1)
+            val leftInorderSubtree = Pair(inorderSubtree.first, indexOfRootValue - 1)
+            val rightInorderSubtree = Pair(indexOfRootValue + 1, inorderSubtree.second)
+            val leftPreorderSubtree = Pair(preorderSubtree.first + 1, preorderSubtree.first + 1 + sizeOf(leftInorderSubtree))
+            val rightPreorderSubtree = Pair(leftPreorderSubtree.second + 1, preorderSubtree.second)
+            TreeNode(rootValue,
+                buildFromPreorderAndInorder(leftPreorderSubtree, leftInorderSubtree),
+                buildFromPreorderAndInorder(rightPreorderSubtree, rightInorderSubtree)
+            )
+        }
+
+    return buildFromPreorderAndInorder(Pair(0, preorder.lastIndex), Pair(0, inorder.lastIndex))
+}
--- src/test/kotlin/org/carlosbello/datastructures/binarytree/OperationsKtTest.kt
+++ src/test/kotlin/org/carlosbello/datastructures/binarytree/OperationsKtTest.kt
@@ -1,28 +1,31 @@
 package org.carlosbello.datastructures.binarytree
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
+import kotlin.streams.asStream
+
+typealias TreeBuilderFunction = (IntArray, IntArray) -> TreeNode?
 
 internal class OperationsKtTest {
 
-    @ParameterizedTest(name = "buildFromPreorderAndPostOrder given {0} and {1}")
+    @ParameterizedTest(name = "Test {0} given {1} and {2}")
     @MethodSource("buildFromInorderAndPostorderTestCases")
-    fun buildFromInorderAndPostOrder(inorder: IntArray, postorder: IntArray, expectedTree: TreeNode?) {
+    fun buildFromInorderAndPostOrder(buildFromInorderAndPostOrderFn: TreeBuilderFunction, inorder: IntArray, postorder: IntArray, expectedTree: TreeNode?) {
         // when
-        val tree = buildFromInorderAndPostorder(inorder, postorder)
+        val tree = buildFromInorderAndPostOrderFn(inorder, postorder)
 
         // then
         assertEquals(expectedTree, tree)
     }
 
-    @ParameterizedTest(name = "buildFromPreorderAndInorder given {0} and {1}")
+    @ParameterizedTest(name = "Test {0} given {1} and {2}")
     @MethodSource("buildFromPreorderAndInorderTestCases")
-    fun buildFromPreorderAndInorder(preorder: IntArray, inorder: IntArray, expectedTree: TreeNode?) {
+    fun buildFromPreorderAndInorder(buildFromPreorderAndInorderFn: TreeBuilderFunction, preorder: IntArray, inorder: IntArray, expectedTree: TreeNode?) {
         // when
-        val tree = buildFromPreorderAndInorder(preorder, inorder)
+        val tree = buildFromPreorderAndInorderFn(preorder, inorder)
 
         // then
         assertEquals(expectedTree, tree)
@@ -32,10 +35,10 @@ internal class OperationsKtTest {
         /**
          * 3
          */
-        val expectedSingleNodeTree = TreeNode(3)
-        val preorderSingleNodeTree = listOf(3).toIntArray()
-        val inorderSingleNodeTree = listOf(3).toIntArray()
-        val postorderSingleNodeTree = listOf(3).toIntArray()
+        private val expectedSingleNodeTree = TreeNode(3)
+        private val preorderSingleNodeTree = listOf(3).toIntArray()
+        private val inorderSingleNodeTree = listOf(3).toIntArray()
+        private val postorderSingleNodeTree = listOf(3).toIntArray()
 
         /**
          *     3
@@ -44,13 +47,13 @@ internal class OperationsKtTest {
          *     /  \
          *    15   7
          */
-        val expectedTree1 = TreeNode(3,
+        private val expectedTree1 = TreeNode(3,
             TreeNode(9),
             TreeNode(20, TreeNode(15), TreeNode(7))
         )
-        val preorderTree1 = listOf(3, 9, 20, 15, 7).toIntArray()
-        val inorderTree1 = listOf(9, 3, 15, 20, 7).toIntArray()
-        val postorderTree1 = listOf(9, 15, 7, 20, 3).toIntArray()
+        private val preorderTree1 = listOf(3, 9, 20, 15, 7).toIntArray()
+        private val inorderTree1 = listOf(9, 3, 15, 20, 7).toIntArray()
+        private val postorderTree1 = listOf(9, 15, 7, 20, 3).toIntArray()
 
         /**
          *      3
@@ -59,30 +62,42 @@ internal class OperationsKtTest {
          *  /  \
          * 15   7
          */
-        val expectedTree2 = TreeNode(3,
+        private val expectedTree2 = TreeNode(3,
             TreeNode(20, TreeNode(15), TreeNode(7)),
             TreeNode(9)
-            )
-        val preorderTree2 = listOf(3, 20, 15, 7, 9).toIntArray()
-        val inorderTree2 = listOf(15, 20, 7, 3, 9).toIntArray()
-        val postorderTree2 = listOf(15, 7, 20, 9, 3).toIntArray()
+        )
+        private val preorderTree2 = listOf(3, 20, 15, 7, 9).toIntArray()
+        private val inorderTree2 = listOf(15, 20, 7, 3, 9).toIntArray()
+        private val postorderTree2 = listOf(15, 7, 20, 9, 3).toIntArray()
+
+        fun mapFunctionToTestCases(functions: Iterable<TreeBuilderFunction>, testCases: Iterable<Array<out Any?>>): Stream<Arguments> =
+            functions.flatMap { fn ->
+                testCases.map { testParams ->
+                    Arguments.of(fn, *testParams)
+                }
+            }.asSequence().asStream()
+
 
         @JvmStatic
         fun buildFromInorderAndPostorderTestCases(): Stream<Arguments> =
-            Stream.of(
-                Arguments.of(IntArray(0), IntArray(0), null),
-                Arguments.of(inorderSingleNodeTree, postorderSingleNodeTree, expectedSingleNodeTree),
-                Arguments.of(inorderTree1, postorderTree1, expectedTree1),
-                Arguments.of(inorderTree2, postorderTree2, expectedTree2)
-            )
+            mapFunctionToTestCases(
+                listOf(::buildFromInorderAndPostorder1, ::buildFromInorderAndPostorder2, ::buildFromInorderAndPostorder3),
+                listOf(
+                    arrayOf(IntArray(0), IntArray(0), null),
+                    arrayOf(inorderSingleNodeTree, postorderSingleNodeTree, expectedSingleNodeTree),
+                    arrayOf(inorderTree1, postorderTree1, expectedTree1),
+                    arrayOf(inorderTree2, postorderTree2, expectedTree2)
+                ))
 
         @JvmStatic
         fun buildFromPreorderAndInorderTestCases(): Stream<Arguments> =
-            Stream.of(
-                Arguments.of(IntArray(0), IntArray(0), null),
-                Arguments.of(preorderSingleNodeTree, inorderSingleNodeTree, expectedSingleNodeTree),
-                Arguments.of(preorderTree1, inorderTree1, expectedTree1),
-                Arguments.of(preorderTree2, inorderTree2, expectedTree2)
-            )
+            mapFunctionToTestCases(
+                listOf(::buildFromPreorderAndInorder1, ::buildFromPreorderAndInorder2, ::buildFromPreorderAndInorder3),
+                listOf(
+                    arrayOf(IntArray(0), IntArray(0), null),
+                    arrayOf(preorderSingleNodeTree, inorderSingleNodeTree, expectedSingleNodeTree),
+                    arrayOf(preorderTree1, inorderTree1, expectedTree1),
+                    arrayOf(preorderTree2, inorderTree2, expectedTree2)
+                ))
     }
 }
