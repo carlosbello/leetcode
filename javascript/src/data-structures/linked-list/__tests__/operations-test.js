1525c38b950c9446c2723d3e3701142f6678987f
Carlos Bello <bellopauste@yahoo.com>
2020-03-15 18:19:28 +0100

    Remove all duplicates from Sorted List solution & tests

--- javascript/src/data-structures/linked-list/__tests__/operations-test.js
+++ javascript/src/data-structures/linked-list/__tests__/operations-test.js
@@ -1,26 +1,36 @@
 const test = require('ava');
-const { deleteDuplicates } = require('../operations');
+const { deleteDuplicates, deleteAllDuplicates } = require('../operations');
 const ListNode = require('../list-node');
 
-const deleteDuplicatesTest = (t, givenList, expectedList) => {
+const deleteDuplicatesTest = (t, deleteDuplicatesFn, givenList, expectedList) => {
     // when
-    deleteDuplicates(givenList);
+    const newList = deleteDuplicatesFn(givenList);
 
     // expects
-    t.deepEqual(expectedList, givenList);
+    t.deepEqual(expectedList, newList);
 };
-deleteDuplicatesTest.title = (t, givenList, expectedList) =>
-    `Test deleteDuplicates returns ${expectedList ? expectedList.toString() : null} given ${
+deleteDuplicatesTest.title = (t, deleteDuplicatesFn, givenList, expectedList) =>
+    `Test ${deleteDuplicatesFn.name} returns ${expectedList ? expectedList.toString() : null} given ${
         givenList ? givenList.toString() : null
     }`;
 
-test(deleteDuplicatesTest, null, null);
-test(deleteDuplicatesTest, new ListNode(1), new ListNode(1));
-test(deleteDuplicatesTest, new ListNode(1, new ListNode(2)), new ListNode(1, new ListNode(2)));
-test(deleteDuplicatesTest, new ListNode(1, new ListNode(1)), new ListNode(1));
-test(deleteDuplicatesTest, new ListNode(1, new ListNode(1, new ListNode(2))), new ListNode(1, new ListNode(2)));
-test(
-    deleteDuplicatesTest,
-    new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(2)))),
-    new ListNode(1, new ListNode(2))
-);
+const list_1 = () => new ListNode(1);
+const list_2 = () => new ListNode(2);
+const list_1_2 = () => new ListNode(1, new ListNode(2));
+const list_1_1 = () => new ListNode(1, new ListNode(1));
+const list_1_1_2 = () => new ListNode(1, new ListNode(1, new ListNode(2)));
+const list_1_1_2_2 = () => new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(2))));
+
+test(deleteDuplicatesTest, deleteDuplicates, null, null);
+test(deleteDuplicatesTest, deleteDuplicates, list_1(), list_1());
+test(deleteDuplicatesTest, deleteDuplicates, list_1_2(), list_1_2());
+test(deleteDuplicatesTest, deleteDuplicates, list_1_1(), list_1());
+test(deleteDuplicatesTest, deleteDuplicates, list_1_1_2(), list_1_2());
+test(deleteDuplicatesTest, deleteDuplicates, list_1_1_2_2(), list_1_2());
+
+test(deleteDuplicatesTest, deleteAllDuplicates, null, null);
+test(deleteDuplicatesTest, deleteAllDuplicates, list_1(), list_1());
+test(deleteDuplicatesTest, deleteAllDuplicates, list_1_2(), list_1_2());
+test(deleteDuplicatesTest, deleteAllDuplicates, list_1_1(), null);
+test(deleteDuplicatesTest, deleteAllDuplicates, list_1_1_2(), list_2());
+test(deleteDuplicatesTest, deleteAllDuplicates, list_1_1_2_2(), null);
--- javascript/src/data-structures/linked-list/operations.js
+++ javascript/src/data-structures/linked-list/operations.js
@@ -20,4 +20,28 @@ const deleteDuplicates = head => {
     return head;
 };
 
-module.exports = { deleteDuplicates };
+/**
+ * 82. Remove Duplicates from Sorted List II https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteAllDuplicates = head => {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    if (head.val === head.next.val) {
+        do {
+            head = head.next;
+        } while (head && (!head.next || head.val === head.next.val));
+        if (head) {
+            head = deleteAllDuplicates(head.next);
+        }
+    } else {
+        head.next = deleteAllDuplicates(head.next);
+    }
+    return head;
+};
+
+module.exports = { deleteDuplicates, deleteAllDuplicates };
