2a17be6da92fc3e4a17accad912c6c464fc69140
Carlos Bello <bellopauste@yahoo.com>
2020-03-19 01:49:26 +0100

    Two Sum IV - Input is a BST solution & tests

--- javascript/src/data-structures/binary-tree/__tests__/operations-test.js
+++ javascript/src/data-structures/binary-tree/__tests__/operations-test.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const { searchBST } = require('../operations');
+const { searchBST, findTwoSumIVInBST } = require('../operations');
 const TreeNode = require('../tree-node');
 
 // @ts-ignore
@@ -9,12 +9,34 @@ test('searchBST return null given null root', t => {
 
 // @ts-ignore
 test('searchBST return null given value is not found', t => {
+    // given
     const tree = new TreeNode(4, new TreeNode(3), new TreeNode(6));
+
+    // then
     t.is(searchBST(tree, 5), null);
 });
 
 // @ts-ignore
 test('searchBST return subtree given existing value', t => {
+    // given
     const tree = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+
+    // then
     t.deepEqual(searchBST(tree, 2), new TreeNode(2, new TreeNode(1), new TreeNode(3)));
+});
+
+// @ts-ignore
+test('findTwoSumIVInBST', t => {
+    // given
+    const bst1 = new TreeNode(
+        5,
+        new TreeNode(3, new TreeNode(2), new TreeNode(4)),
+        new TreeNode(6, null, new TreeNode(7))
+    );
+    const bst2 = new TreeNode(1);
+
+    // then
+    t.is(findTwoSumIVInBST(bst1, 9), true);
+    t.is(findTwoSumIVInBST(bst1, 28), false);
+    t.is(findTwoSumIVInBST(bst2, 2), false);
 });
--- javascript/src/data-structures/binary-tree/operations.js
+++ javascript/src/data-structures/binary-tree/operations.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const TreeNode = require('./tree-node');
+const { inorderTraversal } = require('./traversals');
 
 /**
  * 700. Search in a Binary Search Tree https://leetcode.com/problems/search-in-a-binary-search-tree/
@@ -14,4 +15,22 @@ const searchBST = (root, val) => {
     return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
 };
 
-module.exports = { searchBST };
+/**
+ * 653. Two Sum IV - Input is a BST https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+ *
+ * @param {TreeNode} root
+ * @param {Number} k
+ * @returns {Boolean}
+ */
+const findTwoSumIVInBST = (root, k) => {
+    const values = inorderTraversal(root);
+    let i = 0;
+    let found = false;
+    while (i < values.length && values[i] < k / 2 && !found) {
+        found = !!searchBST(root, k - values[i]);
+        i++;
+    }
+    return found;
+};
+
+module.exports = { searchBST, findTwoSumIVInBST };
