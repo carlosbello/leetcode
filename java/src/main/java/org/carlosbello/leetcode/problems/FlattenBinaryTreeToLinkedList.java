8e0478f3d45f4f6544b30db25d899acbdf3e4d0d
Carlos Bello <bellopauste@yahoo.com>
2021-05-14 09:42:33 +0200

    Add updated solucion to problem 114, in de context of May Challenge 2021

--- java/src/main/java/org/carlosbello/leetcode/problems/FlattenBinaryTreeToLinkedList.java
+++ java/src/main/java/org/carlosbello/leetcode/problems/FlattenBinaryTreeToLinkedList.java
@@ -6,6 +6,27 @@ import org.carlosbello.leetcode.datastructures.binarytree.TreeNode;
  * 114. Flatten Binary Tree to Linked List [medium] https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
  */
 public class FlattenBinaryTreeToLinkedList {
+    
+    /**
+     * Same solution, just a little more compact
+     */
+    public void flatten2(TreeNode root) {
+        if (root == null) return;
+        
+        flatten(root.left);
+        flatten(root.right);
+        
+        if (root.left != null) {
+            var left = root.left;
+            while (left.right != null) {
+                left = left.right;
+            }
+            left.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+    }
+    
     public void flatten(TreeNode root) {
         if (root == null) {
             return;
